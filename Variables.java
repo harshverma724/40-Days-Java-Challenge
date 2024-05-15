0f339abb3ef178772333f00d0fc49a48b784d1e2
Harsh <harshvermajpr1@gmail.com>
2024-05-16 00:25:48 +0530

    Add files via upload
    This file contains essential information about data types in Java programming, focusing on understanding their size and range limitations. It provides insights into the various primitive data types available in Java, such as int, double, float, and boolean, and explains their respective storage capacities and permissible value ranges. Additionally, the file delves into the concept of variables, elucidating their role as placeholders for storing data within a program. Readers will gain a comprehensive understanding of how variables are declared, initialized, and utilized to manipulate data in Java applications. Whether you're a beginner seeking foundational knowledge or an experienced programmer looking for a refresher, this file serves as a valuable resource for mastering the basics of Java data types and variables.

---
+++ Variables.java
@@ -0,0 +1,74 @@
+import java.lang.*;
+
+public class Variables{
+
+	public static void main(String[] args)
+	{
+
+		System.out.println("Declaring a variable");
+		byte a = 28;
+		short b = 690;
+		int c = 101;
+		long d = 293452462;
+		float e = 154.02f;
+		double f = 3.12;
+		char g = 'A';
+		String h = "Harsh Verma";
+		Boolean i = true;
+
+		System.out.println("Byte a value: "+a);
+		System.out.println("Short b value: "+b);
+		System.out.println("Integer c value: "+b);
+		System.out.println("Long d value: "+d);
+		System.out.println("Float e value: "+e);
+		System.out.println("Double f value: "+f);
+		System.out.println("Character g value: "+g);
+		System.out.println("String h value: "+h);
+		System.out.println("Boolean i value: "+i);
+
+		System.out.println(" ");
+		System.out.println("About Datatypes: ");
+		System.out.println(" ");
+
+		System.out.println("Integer Min Value: "+Integer.MIN_VALUE);
+		System.out.println("Integer Max Value: "+Integer.MAX_VALUE);
+		System.out.println("Integer Bytes Taken: "+Integer.BYTES);
+		System.out.println("Integer Size Taken: "+Integer.SIZE);
+		
+		System.out.println(" ");
+
+		System.out.println("Float Min Value: "+Float.MIN_VALUE);
+		System.out.println("Float Max Value: "+Float.MAX_VALUE);
+		System.out.println("Float Bytes Taken: "+Float.BYTES);
+		System.out.println("Float Size Taken: "+Float.SIZE);
+
+		System.out.println(" ");
+
+		System.out.println("Short Min Value: "+Short.MIN_VALUE);
+		System.out.println("Short Max Value: "+Short.MAX_VALUE);
+		System.out.println("Short Bytes Taken: "+Short.BYTES);
+		System.out.println("Short Size Taken: "+Short.SIZE);
+
+		System.out.println(" ");
+
+		System.out.println("Long Min Value: "+Long.MIN_VALUE);
+		System.out.println("Long Max Value: "+Long.MAX_VALUE);
+		System.out.println("Long Bytes Taken: "+Long.BYTES);
+		System.out.println("Long Size Taken: "+Long.SIZE);
+
+		System.out.println(" ");
+
+		System.out.println("Byte Min Value: "+Byte.MIN_VALUE);
+		System.out.println("Byte Max Value: "+Byte.MAX_VALUE);
+		System.out.println("Byte Bytes Taken: "+Byte.BYTES);
+		System.out.println("Byte Size Taken: "+Byte.SIZE);
+
+		System.out.println(" ");
+
+		System.out.println("Double Min Value: "+Double.MIN_VALUE);
+		System.out.println("Double Max Value: "+Double.MAX_VALUE);
+		System.out.println("Double Bytes Taken: "+Double.BYTES);
+		System.out.println("Double Size Taken: "+Double.SIZE);
+	
+	}
+}
