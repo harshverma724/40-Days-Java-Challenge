1fb7797a0080c50d399a84e971f1e86f072df9ea
Harsh <harshvermajpr1@gmail.com>
2024-05-16 22:53:11 +0530

    Add files via upload
    Check Binary Bits of an Integer

---
+++ Binary.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.util.*;
+public class Binary{
+
+	public static void main(String[] args)
+	{
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number here: ");
+		int a = sc.nextInt();
+		
+
+		System.out.println("Binary Number of "+a+" is "+": "+Integer.toBinaryString(a));
+	
+	}
+}
