869a6c67365d267ea8ad9c7ed0f254928d8de384
Harsh <harshvermajpr1@gmail.com>
2024-05-19 21:31:11 +0530

    Add files via upload

---
+++ Day_6/bitwise.java
@@ -0,0 +1,13 @@
+public class bitwise {
+    public static void main(String[] args) {
+        int x = 10, y = 6, z;
+
+        System.out.println("Bitwise And " + (x & y));
+        System.out.println("Bitwise OR " + (x | y));
+        System.out.println("Bitwise XOR " + (x ^ y));
+        System.out.println("Bitwise Right Shift " + (x >> 1));
+        System.out.println("Bitwise Left Shift " + (x << 1));
+
+    }
+
+}
