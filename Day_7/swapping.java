98001484d023aeb02b101ca5c9d0958ea79c3ad6
Harsh <harshvermajpr1@gmail.com>
2024-05-20 22:19:47 +0530

    Add files via upload

---
+++ Day_7/swapping.java
@@ -0,0 +1,12 @@
+public class swapping {
+    public static void main(String[] args) {
+        byte a = 4, b = 7;
+
+        a = (byte) (a ^ b);
+        b = (byte) (a ^ b);
+        a = (byte) (a ^ b);
+
+        System.out.println(a + " " + b);
+    }
+
+}
