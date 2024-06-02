f6e3ec3ed783a6a16e24a0334caf52d6cc19be67
Harsh <harshvermajpr1@gmail.com>
2024-06-02 23:32:33 +0530

    Add files via upload

---
+++ Day__20/inheritanceTwo.java
@@ -0,0 +1,56 @@
+class CellPhone {
+
+    private String phoneno;
+    private String imeino;
+
+    public String getPhoneNo() {
+        return phoneno;
+
+    }
+
+    public String getImeiNo() {
+        return imeino;
+
+    }
+
+    public void makeCall() {
+
+    }
+
+    public void sendSms() {
+
+    }
+
+    public void AddContact() {
+
+    }
+
+    public void DelContect() {
+
+    }
+
+}
+
+class Smartphone extends CellPhone {
+    private String macno;
+
+    public void play() {
+
+    }
+
+    public void click() {
+
+    }
+
+    public void capture() {
+
+    }
+
+}
+
+public class inheritanceTwo {
+    public static void main(String[] args) {
+
+    }
+
+}
