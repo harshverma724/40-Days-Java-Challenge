8a1ca8c77b90280913f1821947affe013ada7db1
Harsh <harshvermajpr1@gmail.com>
2024-06-10 23:30:02 +0530

    Add files via upload

---
+++ Day__28/javadocDemo.java
@@ -0,0 +1,65 @@
+/**
+ * @author Harsh Verma
+ * @since 2024
+ * @version 1.0
+ **/
+
+/**
+ * @author harshverma
+ * 
+ * 
+ *         Class For Library Program.
+ **/
+
+public class javadocDemo {
+
+    /**
+     * Parametrized Constructor.
+     * 
+     * @value 10 default value.
+     */
+
+    static int val = 10;
+
+    /**
+     * 
+     * @param s Book Name.
+     */
+
+    public javadocDemo(String s) {
+
+    }
+
+    /**
+     * Issue a Book to a Student.
+     * 
+     * @param rollno Roll No. of a Student
+     * @throws Exception IF Book not available then throws Exception
+     */
+    public void issue(int rollno) throws Exception {
+
+    }
+
+    /**
+     * Check availability of Book
+     * 
+     * @param Id Book Name.
+     * @return If book available then return true else false
+     */
+
+    public boolean available(String Id) {
+        return true;
+    }
+
+    /**
+     * Get Book Name.
+     * 
+     * @param Id Book Id.
+     * @return returns Book Name.
+     */
+
+    public String getName(int Id) {
+        return "";
+    }
+
+}
