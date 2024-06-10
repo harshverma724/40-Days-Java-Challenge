717ad46aaa60f1166c368ae68d24d8c5f40da3b6
Harsh <harshvermajpr1@gmail.com>
2024-06-10 23:30:33 +0530

    Add files via upload

---
+++ Day__28/annotations.java
@@ -0,0 +1,19 @@
+class Demo {
+    public void display() {
+
+    }
+}
+
+class Child extends Demo {
+    @Override
+    public void display() {
+
+    }
+}
+
+public class annotations {
+    public static void main(String[] args) {
+
+    }
+
+}
